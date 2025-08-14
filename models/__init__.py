99d3cc328f021bd7dedae894a53f008071fe2502
ardhanapw <68503796+ardhanapw@users.noreply.github.com>
2025-08-14 13:12:06 +0700

    added lite-mono architecture

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,14 @@
 from models.monodepth2.model import MonoDepth2
 from models.depth_hints.model import DepthHints
-from models.monovit.model import MonoViT
+#from models.monovit.model import MonoViT
+from models.lite_mono.model import LiteMono
 
 def load_models(model_name, model_path, device):
     if model_name == 'monodepth2':
         return MonoDepth2(model_path, device)
     elif model_name == 'depth_hints':
         return DepthHints(model_path, device)
-    elif model_name == 'monovit':
-        return MonoViT(model_path, device)
+    #elif model_name == 'monovit':
+    #    return MonoViT(model_path, device)
+    elif model_name == 'lite_mono':
+        return LiteMono(model_path, device)
