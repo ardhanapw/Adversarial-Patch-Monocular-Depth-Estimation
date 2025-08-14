30b1ab4f6a24bb675966e2119aaa64c83d0f012c
ardhanapw <68503796+ardhanapw@users.noreply.github.com>
2025-08-14 08:19:27 +0700

    monovit
    not tested yet on inference

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,11 @@
 from models.monodepth2.model import MonoDepth2
 from models.depth_hints.model import DepthHints
+from models.monovit.model import MonoViT
 
 def load_models(model_name, model_path, device):
     if model_name == 'monodepth2':
         return MonoDepth2(model_path, device)
     elif model_name == 'depth_hints':
         return DepthHints(model_path, device)
+    elif model_name == 'monovit':
+        return MonoViT(model_path, device)
