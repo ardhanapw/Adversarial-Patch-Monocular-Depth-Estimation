9bc1b37b67c141fe364bd0dfd8e50c095768b197
ardhanapw <68503796+ardhanapw@users.noreply.github.com>
2025-08-16 13:36:28 +0700

    mpvit for monovit (monovit is not used)

--- models/monovit/networks/mpvit.py
+++ models/monovit/networks/mpvit.py
@@ -26,10 +26,13 @@ from torch import nn, einsum
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from mmengine.runner import load_checkpoint,load_state_dict
+from mmengine.logging import MMLogger #test
 from mmcv.cnn import build_norm_layer
 
-from mmseg.utils import get_root_logger
 from mmseg.models.builder import BACKBONES
+
+def get_root_logger():
+    return MMLogger.get_instance(name='mmengine')
 
 __all__ = [
     "mpvit_tiny",
